9ea7881796c82421715c6f6e8277c7a08ff7b9d1
jsantiago240 <jvsjgs123@gmail.com>
2018-07-02 02:42:06 -0400

    Video 6 Done

--- core/src/com/javiussantiago/mariobros/screens/PlayScreen.java
+++ core/src/com/javiussantiago/mariobros/screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -19,12 +22,36 @@ public class PlayScreen implements Screen
     private Viewport gamePort;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+
     public PlayScreen(MarioBros game)
     {
         this.game = game;
-        gameCam = new OrthographicCamera();
-        gamePort = new FitViewport(MarioBros.V_WIDTH,MarioBros.V_HEIGHT,gameCam);
+
+        gameCam = new OrthographicCamera(); //follows mario
+        gamePort = new FitViewport(MarioBros.V_WIDTH,MarioBros.V_HEIGHT,gameCam);   //maintains aspect ratios
         hud = new Hud(game.batch);
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1cleaned.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+    }
+
+    public void handleInput(float dt)
+    {
+        if(Gdx.input.isTouched())
+            gameCam.position.x += 100 * dt;
+    }
+
+    public void update(float dt)
+    {
+        handleInput(dt);  //check input
+        gameCam.update();   //update camera
+        renderer.setView(gameCam);
+
     }
 
     @Override
@@ -34,13 +61,15 @@ public class PlayScreen implements Screen
     }
 
     @Override
-    public void render(float delta)
+    public void render(float dt)
     {
-        Gdx.gl.glClearColor(1,0,0,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        update(dt);
+        /*dx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);*/
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
+        renderer.render();
     }
 
     @Override
