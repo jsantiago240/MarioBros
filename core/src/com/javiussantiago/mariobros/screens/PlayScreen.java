cd8c7f3ac1963ba01d89868722841dd746638154
jsantiago240 <jvsjgs123@gmail.com>
2018-07-02 01:40:28 -0400

    Video 3 Done

--- core/src/com/javiussantiago/mariobros/screens/PlayScreen.java
+++ core/src/com/javiussantiago/mariobros/screens/PlayScreen.java
@@ -3,22 +3,30 @@ package com.javiussantiago.mariobros.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.javiussantiago.mariobros.MarioBros;
 
 public class PlayScreen implements Screen
 {
     private MarioBros game;
-    private Texture texture;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
 
     public PlayScreen(MarioBros game)
     {
         this.game = game;
-        texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(800,480,gameCam);
     }
 
     @Override
-    public void show() {
+    public void show()
+    {
 
     }
 
@@ -27,33 +35,38 @@ public class PlayScreen implements Screen
     {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
         game.batch.begin();
-        game.batch.draw(texture, 0, 0);
         game.batch.end();
     }
 
     @Override
-    public void resize(int width, int height) {
+    public void resize(int width, int height)
+    {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause()
+    {
 
     }
 
     @Override
-    public void pause() {
+    public void resume()
+    {
 
     }
 
     @Override
-    public void resume() {
+    public void hide()
+    {
 
     }
 
     @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
+    public void dispose()
+    {
 
     }
 }
