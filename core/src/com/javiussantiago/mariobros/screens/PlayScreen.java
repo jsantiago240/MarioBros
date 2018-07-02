8a874fac963d4b14167bd81baf1e54f14bffc0c5
jsantiago240 <jvsjgs123@gmail.com>
2018-07-02 01:21:33 -0400

    1:21am 7/2

---
+++ core/src/com/javiussantiago/mariobros/screens/PlayScreen.java
@@ -0,0 +1,49 @@
+package com.javiussantiago.mariobros.screens;
+
+import com.badlogic.gdx.Screen;
+import com.javiussantiago.mariobros.MarioBros;
+
+public class PlayScreen implements Screen
+{
+    private MarioBros game;
+
+    public PlayScreen(MarioBros game)
+    {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
